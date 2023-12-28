c88b6baebfc770dcb3403f934f9b9ba7466673fd
Nahuel Rodriguez <nahuetomas@outlook.com>
2023-12-28 07:50:29 -0300

    [ADD] Query - Microservice

---
+++ query/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import axios from 'axios'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body
+  
+  if(type === 'PostCreated') {
+    const { id, title} = data
+    posts[id] = { id, title, comments: [] }
+  }
+
+  else if(type === 'CommentCreated') {
+    const { id, content, postId } = data
+    posts[postId].comments.push({ id, content })
+  }
+
+  res.status(201).end()
+})
+
+app.listen(4002, () => {
+  console.log('Listen on por 4002')
+})
