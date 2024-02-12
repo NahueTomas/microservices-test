48fa672f6976f344cef64757adfd4cc8a2278c39
Nahuel Rodriguez <nahuetomas@outlook.com>
2024-02-11 21:18:09 -0300

    [UPDATE] Routes

--- client/src/services/api/index.js
+++ client/src/services/api/index.js
@@ -1,7 +1,7 @@
 // Variables
 export const URL_POSTS = process.env.REACT_APP_POSTS_URL
 export const GET_POSTS = `${URL_POSTS}/posts`
-export const POST_POST = `${URL_POSTS}/posts`
+export const POST_POST = `${URL_POSTS}/posts/create`
 
 export const URL_COMMENTS = `${process.env.REACT_APP_COMMENTS_URL}/posts`
 
